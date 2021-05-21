27cc2e0fbf5c1dee062ef1a6b7fa01e6122e1cb2
Hadi Kachmar <HADIKACHMAR8@GMAIL.COM>
2021-05-21 20:00:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Food Ordering App UI
+  An online food ordering app UI created in Flutter
+  
+## Design Inspiration
+- [Link](https://www.instagram.com/p/B4ns6R3AAFO/?igshid=13girogl1ddzj)
+
+## ScreenShots
+
+<br>
+
+<img height=600 width=300 src="https://github.com/usman18/Flutter-UI-Kit/blob/master/%233_food_ordering_app_ui/Screenshots/foodorder1.jpg" hspace=100/><img height=600 width=300 src="https://github.com/usman18/Flutter-UI-Kit/blob/master/%233_food_ordering_app_ui/Screenshots/foodorder2.jpg" />
+ 
+## Contributions
+Contributions are always welcome. Please fork this repository and contribute using pull requests. The pull requests will be thoroughly assessed and if found significant will be accepted.
+
+## Lets become friends
+- [Medium](https://medium.com/@usman18)
+- [Instagram](https://www.instagram.com/usman__khan18)
+- [Twitter](https://www.twitter.com/khan_usman_18)
+- [LinkedIn](https://www.linkedin.com/in/usman-khan-7b04b1138)
+- [Github](https://github.com/usman18)
+
+My email : uk32971@gmail.com
---
+++ lib/providers/food_items_provider.dart
@@ -0,0 +1,15 @@
+
+import 'package:food_ordering_app_ui/model/food_item.dart';
+
+class FoodItemsProvider {
+
+  static final List<FoodItem> items = [
+    FoodItem(itemName: "Pear & Orange", estimatedTime: "20 min", rating: "4.8", price: "25.00", imageUrl: "https://assets.andrewmartin.co.uk/images/square-grey/75549-pearglazedceramicorange.jpg"),
+    FoodItem(itemName: "Meat & Mushrooms", estimatedTime: "30 min", rating: "5.0", price: "37.00", imageUrl: "https://salu-salo.com/wp-content/uploads/2015/04/Beef-and-Mushroom-Stir-Fry-3.jpg"),
+    FoodItem(itemName: "Egg & Bread", estimatedTime: "10 min", rating: "4.7", price: "25.00", imageUrl: "https://img-global.cpcdn.com/recipes/643c3fde46cd645e/1200x630cq70/photo.jpg"),
+    FoodItem(itemName: "Sweet pancake", estimatedTime: "10 min", rating: "4.9", price: "13.00", imageUrl: "https://redtri.com/wp-content/uploads/2021/05/cucinopro-unicorn-mini-pancake-pan-1619642268.jpg?w=980"),
+    FoodItem(itemName: "Meat & Mushrooms", estimatedTime: "30 min", rating: "5.0", price: "37.00", imageUrl: "https://salu-salo.com/wp-content/uploads/2015/04/Beef-and-Mushroom-Stir-Fry-3.jpg"),
+    FoodItem(itemName: "Sweet pancake", estimatedTime: "10 min", rating: "4.9", price: "13.00", imageUrl: "https://www.dontsweattherecipe.com/wp-content/uploads/2016/06/lolaPancakes-06.jpg"),
+
+  ];
+}
